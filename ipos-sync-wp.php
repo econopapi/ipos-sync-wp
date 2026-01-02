2c1622c93cd5170fd430e97c0000c9cc24cb77b4
Daniel Limón <dlimon.munoz@gmail.com>
2026-01-02 13:26:27 -0600

    add: Plugin info. Primer paso completado.

---
+++ ipos-sync-wp.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: iPos Sync WP
+ * Description: Integración completa de iPos con WordPress/WooCommerce.
+ * Version: 1.0.0
+ * Author: Daniel Limón
+ * Author URI: https://dlimon.net
+ * Licence: Pendiente
+ */
